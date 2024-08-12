a4125b93a098ef691522e491247eaf3450287ba9
Svdex <renixiel@gmail.com>
2024-08-12 12:22:59 +0700

    Add company

---
+++ src/routes/v1/company.rs
@@ -0,0 +1,172 @@
+use crate::{
+    db::DbPool,
+    models::Company,
+    response::{ErrorResponse, OkResponse},
+};
+use actix_multipart::form::{text::Text, MultipartForm};
+use actix_web::{
+    get,
+    http::StatusCode,
+    post,
+    web::{self, Data, ServiceConfig},
+    HttpRequest, HttpResponse, Result,
+};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Query {
+    id: Option<i64>,
+}
+
+#[get("")]
+async fn get_company(req: HttpRequest, data: Data<DbPool>) -> Result<HttpResponse, ErrorResponse> {
+    use crate::schema::company::dsl::*;
+
+    let mut connection = match data.get() {
+        Ok(conn) => conn,
+        Err(e) => {
+            return Err(ErrorResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to get db connection from pool: {}", e),
+                Some("db_connection_failed".to_string()),
+            ));
+        }
+    };
+    let query = web::Query::<Query>::from_query(req.query_string()).unwrap();
+    if let Some(i) = query.id {
+        let result = company.find(i).first::<Company>(&mut connection);
+        if let Ok(comp) = result {
+            let comp: Company = comp;
+            return Ok(OkResponse::new(
+                "Company found".to_string(),
+                Some(serde_json::to_value(comp).unwrap()),
+            ));
+        } else {
+            return Err(ErrorResponse::new(
+                StatusCode::NOT_FOUND,
+                "Company not found".to_string(),
+                Some("company_not_found".to_string()),
+            ));
+        }
+    } else {
+        return Err(ErrorResponse::new(
+            StatusCode::BAD_REQUEST,
+            "Invalid query".to_string(),
+            Some("invalid_query".to_string()),
+        ));
+    }
+}
+
+#[derive(Debug, MultipartForm)]
+struct CompanyForm {
+    name: Option<Text<String>>,
+}
+
+#[post("")]
+async fn add_company(
+    MultipartForm(form): MultipartForm<CompanyForm>,
+    data: Data<DbPool>,
+) -> Result<HttpResponse, ErrorResponse> {
+    use crate::schema::company::dsl::*;
+
+    let mut connection = match data.get() {
+        Ok(conn) => conn,
+        Err(e) => {
+            return Err(ErrorResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to get db connection from pool: {}", e),
+                Some("db_connection_failed".to_string()),
+            ));
+        }
+    };
+    let company_name = match form.name {
+        Some(n) => n.into_inner(),
+        None => {
+            return Err(ErrorResponse::new(
+                StatusCode::BAD_REQUEST,
+                "Company name is required".to_string(),
+                Some("company_name_required".to_string()),
+            ));
+        }
+    };
+    match diesel::insert_into(company)
+        .values(Company {
+            name: company_name,
+            ..Default::default()
+        })
+        .execute(&mut connection)
+    {
+        Ok(_) => Ok(OkResponse::new("Company added".to_string(), None)),
+        Err(e) => Err(ErrorResponse::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to add company: {}", e),
+            Some("company_add_failed".to_string()),
+        )),
+    }
+}
+
+#[derive(MultipartForm)]
+struct CompanyUpdateForm {
+    id: Option<Text<i64>>,
+    name: Option<Text<String>>,
+}
+
+#[post("/update")]
+async fn update(
+    MultipartForm(form): MultipartForm<CompanyUpdateForm>,
+    data: Data<DbPool>,
+) -> Result<HttpResponse, ErrorResponse> {
+    use crate::schema::company::dsl::*;
+
+    let mut connection = match data.get() {
+        Ok(conn) => conn,
+        Err(e) => {
+            return Err(ErrorResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to get db connection from pool: {}", e),
+                Some("db_connection_failed".to_string()),
+            ));
+        }
+    };
+    let company_id = match form.id {
+        Some(i) => i.into_inner(),
+        None => {
+            return Err(ErrorResponse::new(
+                StatusCode::BAD_REQUEST,
+                "Company id is required".to_string(),
+                Some("company_id_required".to_string()),
+            ));
+        }
+    };
+    let company_name = match form.name {
+        Some(n) => n.into_inner(),
+        None => {
+            return Err(ErrorResponse::new(
+                StatusCode::BAD_REQUEST,
+                "Company name is required".to_string(),
+                Some("company_name_required".to_string()),
+            ));
+        }
+    };
+    match diesel::update(company.find(company_id))
+        .set(name.eq(company_name))
+        .execute(&mut connection)
+    {
+        Ok(_) => Ok(OkResponse::new("Company updated".to_string(), None)),
+        Err(e) => Err(ErrorResponse::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to update company: {}", e),
+            Some("company_update_failed".to_string()),
+        )),
+    }
+}
+
+pub fn init(config: &mut ServiceConfig) {
+    config.service(
+        web::scope("/company")
+            .service(get_company)
+            .service(add_company)
+            .service(update),
+    );
+}
--- src/routes/v1/init.rs
+++ src/routes/v1/init.rs
@@ -1,6 +1,10 @@
-use super::user;
+use super::{company, user};
 use actix_web::web::{self, ServiceConfig};
 
 pub fn init(cfg: &mut ServiceConfig) {
-    cfg.service(web::scope("/v1").configure(user::init));
+    cfg.service(
+        web::scope("/v1")
+            .configure(user::init)
+            .configure(company::init),
+    );
 }
