bd3b8ffe59dfc84c8f2b29e02959e9dfb6e64cb4
Svdex <renixiel@gmail.com>
2024-08-12 16:18:05 +0700

    Add unfollow

--- src/routes/v1/follow.rs
+++ src/routes/v1/follow.rs
@@ -86,6 +86,77 @@ async fn follow(
     Ok(HttpResponse::Ok().finish())
 }
 
+#[post("/unfollow")]
+async fn unfollow(
+    form: web::Json<FollowForm>,
+    data: Data<DbPool>,
+) -> Result<HttpResponse, ErrorResponse> {
+    use crate::schema::follows::dsl::*;
+    use crate::schema::users::dsl::*;
+
+    if form.user_id == form.followed_user_id {
+        return Err(ErrorResponse::new(
+            StatusCode::BAD_REQUEST,
+            "User cannot unfollow themselves".to_string(),
+            Some("user_unfollowing_self".to_string()),
+        ));
+    }
+
+    let mut connection = match data.get() {
+        Ok(conn) => conn,
+        Err(e) => {
+            return Err(ErrorResponse::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to get db connection from pool: {}", e),
+                Some("db_connection_failed".to_string()),
+            ));
+        }
+    };
+    let following_user = match users.find(form.user_id).first::<User>(&mut connection) {
+        Ok(u) => {
+            let u: User = u;
+            u
+        }
+        Err(_) => {
+            return Err(ErrorResponse::new(
+                StatusCode::NOT_FOUND,
+                "User not found".to_string(),
+                Some("user_not_found".to_string()),
+            ));
+        }
+    };
+    let followed_user = match users
+        .find(form.followed_user_id)
+        .first::<User>(&mut connection)
+    {
+        Ok(u) => {
+            let u: User = u;
+            u
+        }
+        Err(_) => {
+            return Err(ErrorResponse::new(
+                StatusCode::NOT_FOUND,
+                "User not found".to_string(),
+                Some("user_not_found".to_string()),
+            ));
+        }
+    };
+    diesel::delete(
+        follows
+            .filter(following_user_id.eq(following_user.id.unwrap()))
+            .filter(followed_user_id.eq(followed_user.id.unwrap())),
+    )
+    .execute(&mut connection)
+    .map_err(|e| {
+        ErrorResponse::new(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to unfollow user: {}", e),
+            Some("unfollow_user_failed".to_string()),
+        )
+    })?;
+    Ok(HttpResponse::Ok().finish())
+}
+
 pub fn init(config: &mut ServiceConfig) {
-    config.service(follow);
+    config.service(follow).service(unfollow);
 }
